b97d6e15bd0956aeb1d4ceae901ee5e1e51679ea
Listiarso Wastuargo <hallucinogenplus@gmail.com>
2015-09-23 14:05:17 -0700

    Add more tests on UserTests

--- ParseTest.Unit/UserTests.cs
+++ ParseTest.Unit/UserTests.cs
@@ -210,15 +210,60 @@ namespace ParseTest {
     [Test]
     [AsyncStateMachine(typeof(UserTests))]
     public Task TestLogIn() {
-      // TODO (hallucinogen): do this
-      return Task.FromResult(0);
+      IObjectState state = new MutableObjectState {
+        ServerData = new Dictionary<string, object>() {
+          { "sessionToken", "llaKcolnu" },
+          { "username", "ihave" },
+          { "password", "adream" }
+        }
+      };
+      IObjectState newState = new MutableObjectState {
+        ObjectId = "some0neTol4v4"
+      };
+      var mockController = new Mock<IParseUserController>();
+      mockController.Setup(obj => obj.LogInAsync("ihave",
+          "adream",
+          It.IsAny<CancellationToken>())).Returns(Task.FromResult(newState));
+      ParseCorePlugins.Instance.UserController = mockController.Object;
+
+      return ParseUser.LogInAsync("ihave", "adream").ContinueWith(t => {
+        Assert.False(t.IsFaulted);
+        Assert.False(t.IsCanceled);
+        mockController.Verify(obj => obj.LogInAsync("ihave",
+          "adream",
+          It.IsAny<CancellationToken>()), Times.Exactly(1));
+
+        var user = t.Result;
+        Assert.False(user.IsDirty);
+        Assert.Null(user.Username);
+        Assert.AreEqual("some0neTol4v4", user.ObjectId);
+      });
     }
 
     [Test]
     [AsyncStateMachine(typeof(UserTests))]
     public Task TestBecome() {
-      // TODO (hallucinogen): do this
-      return Task.FromResult(0);
+      IObjectState state = new MutableObjectState {
+        ObjectId = "some0neTol4v4",
+        ServerData = new Dictionary<string, object>() {
+          { "sessionToken", "llaKcolnu" }
+        }
+      };
+      var mockController = new Mock<IParseUserController>();
+      mockController.Setup(obj => obj.GetUserAsync("llaKcolnu",
+          It.IsAny<CancellationToken>())).Returns(Task.FromResult(state));
+      ParseCorePlugins.Instance.UserController = mockController.Object;
+
+      return ParseUser.BecomeAsync("llaKcolnu").ContinueWith(t => {
+        Assert.False(t.IsFaulted);
+        Assert.False(t.IsCanceled);
+        mockController.Verify(obj => obj.GetUserAsync("llaKcolnu",
+          It.IsAny<CancellationToken>()), Times.Exactly(1));
+
+        var user = t.Result;
+        Assert.AreEqual("some0neTol4v4", user.ObjectId);
+        Assert.AreEqual("llaKcolnu", user.SessionToken);
+      });
     }
 
     [Test]
@@ -311,40 +356,238 @@ namespace ParseTest {
       });
     }
 
-
     [Test]
     [AsyncStateMachine(typeof(UserTests))]
     public Task TestUserSave() {
-      // TODO (hallucinogen): do this
-      return Task.FromResult(0);
+      IObjectState state = new MutableObjectState {
+        ObjectId = "some0neTol4v4",
+        ServerData = new Dictionary<string, object>() {
+          { "sessionToken", "llaKcolnu" },
+          { "username", "ihave" },
+          { "password", "adream" }
+        }
+      };
+      IObjectState newState = new MutableObjectState {
+        ServerData = new Dictionary<string, object>() {
+          { "Alliance", "rekt" }
+        }
+      };
+      ParseUser user = ParseObject.FromState<ParseUser>(state, "_User");
+      var mockObjectController = new Mock<IParseObjectController>();
+      mockObjectController.Setup(obj => obj.SaveAsync(It.IsAny<IObjectState>(),
+          It.IsAny<IDictionary<string, IParseFieldOperation>>(),
+          It.IsAny<string>(),
+          It.IsAny<CancellationToken>())).Returns(Task.FromResult(newState));
+      ParseCorePlugins.Instance.ObjectController = mockObjectController.Object;
+      ParseCorePlugins.Instance.CurrentUserController = new Mock<IParseCurrentUserController>().Object;
+      user["Alliance"] = "rekt";
+
+      return user.SaveAsync().ContinueWith(t => {
+        Assert.False(t.IsFaulted);
+        Assert.False(t.IsCanceled);
+        mockObjectController.Verify(obj => obj.SaveAsync(It.IsAny<IObjectState>(),
+          It.IsAny<IDictionary<string, IParseFieldOperation>>(),
+          It.IsAny<string>(),
+          It.IsAny<CancellationToken>()), Times.Exactly(1));
+        Assert.False(user.IsDirty);
+        Assert.AreEqual("ihave", user.Username);
+        Assert.False(user.State.ContainsKey("password"));
+        Assert.AreEqual("some0neTol4v4", user.ObjectId);
+        Assert.AreEqual("rekt", user["Alliance"]);
+      });
     }
 
     [Test]
     [AsyncStateMachine(typeof(UserTests))]
     public Task TestUserFetch() {
-      // TODO (hallucinogen): do this
-      return Task.FromResult(0);
+      IObjectState state = new MutableObjectState {
+        ObjectId = "some0neTol4v4",
+        ServerData = new Dictionary<string, object>() {
+          { "sessionToken", "llaKcolnu" },
+          { "username", "ihave" },
+          { "password", "adream" }
+        }
+      };
+      IObjectState newState = new MutableObjectState {
+        ServerData = new Dictionary<string, object>() {
+          { "Alliance", "rekt" }
+        }
+      };
+      ParseUser user = ParseObject.FromState<ParseUser>(state, "_User");
+      var mockObjectController = new Mock<IParseObjectController>();
+      mockObjectController.Setup(obj => obj.FetchAsync(It.IsAny<IObjectState>(),
+          It.IsAny<string>(),
+          It.IsAny<CancellationToken>())).Returns(Task.FromResult(newState));
+      ParseCorePlugins.Instance.ObjectController = mockObjectController.Object;
+      ParseCorePlugins.Instance.CurrentUserController = new Mock<IParseCurrentUserController>().Object;
+      user["Alliance"] = "rekt";
+
+      return user.FetchAsync().ContinueWith(t => {
+        Assert.False(t.IsFaulted);
+        Assert.False(t.IsCanceled);
+        mockObjectController.Verify(obj => obj.FetchAsync(It.IsAny<IObjectState>(),
+          It.IsAny<string>(),
+          It.IsAny<CancellationToken>()), Times.Exactly(1));
+        Assert.True(user.IsDirty);
+        Assert.AreEqual("ihave", user.Username);
+        Assert.True(user.State.ContainsKey("password"));
+        Assert.AreEqual("some0neTol4v4", user.ObjectId);
+        Assert.AreEqual("rekt", user["Alliance"]);
+      });
     }
 
     [Test]
     [AsyncStateMachine(typeof(UserTests))]
     public Task TestLink() {
-      // TODO (hallucinogen): do this
-      return Task.FromResult(0);
+      IObjectState state = new MutableObjectState {
+        ObjectId = "some0neTol4v4",
+        ServerData = new Dictionary<string, object>() {
+          { "sessionToken", "llaKcolnu" }
+        }
+      };
+      IObjectState newState = new MutableObjectState {
+        ServerData = new Dictionary<string, object>() {
+          { "garden", "ofWords" }
+        }
+      };
+      ParseUser user = ParseObject.FromState<ParseUser>(state, "_User");
+      var mockObjectController = new Mock<IParseObjectController>();
+      mockObjectController.Setup(obj => obj.SaveAsync(It.IsAny<IObjectState>(),
+          It.IsAny<IDictionary<string, IParseFieldOperation>>(),
+          It.IsAny<string>(),
+          It.IsAny<CancellationToken>())).Returns(Task.FromResult(newState));
+      ParseCorePlugins.Instance.ObjectController = mockObjectController.Object;
+      ParseCorePlugins.Instance.CurrentUserController = new Mock<IParseCurrentUserController>().Object;
+
+      return user.LinkWithAsync("parse", new Dictionary<string, object>(), CancellationToken.None).ContinueWith(t => {
+        Assert.False(t.IsFaulted);
+        Assert.False(t.IsCanceled);
+        mockObjectController.Verify(obj => obj.SaveAsync(It.IsAny<IObjectState>(),
+          It.IsAny<IDictionary<string, IParseFieldOperation>>(),
+          It.IsAny<string>(),
+          It.IsAny<CancellationToken>()), Times.Exactly(1));
+        Assert.False(user.IsDirty);
+        Assert.NotNull(user.AuthData);
+        Assert.NotNull(user.AuthData["parse"]);
+        Assert.AreEqual("some0neTol4v4", user.ObjectId);
+        Assert.AreEqual("ofWords", user["garden"]);
+      });
     }
 
     [Test]
     [AsyncStateMachine(typeof(UserTests))]
     public Task TestUnlink() {
-      // TODO (hallucinogen): do this
-      return Task.FromResult(0);
+      IObjectState state = new MutableObjectState {
+        ObjectId = "some0neTol4v4",
+        ServerData = new Dictionary<string, object>() {
+          { "sessionToken", "llaKcolnu" },
+          { "authData", new Dictionary<string, object> {
+            { "parse", new Dictionary<string, object>() }
+          }}
+        }
+      };
+      IObjectState newState = new MutableObjectState {
+        ServerData = new Dictionary<string, object>() {
+          { "garden", "ofWords" }
+        }
+      };
+      ParseUser user = ParseObject.FromState<ParseUser>(state, "_User");
+      var mockObjectController = new Mock<IParseObjectController>();
+      mockObjectController.Setup(obj => obj.SaveAsync(It.IsAny<IObjectState>(),
+          It.IsAny<IDictionary<string, IParseFieldOperation>>(),
+          It.IsAny<string>(),
+          It.IsAny<CancellationToken>())).Returns(Task.FromResult(newState));
+      var mockCurrentUserController = new Mock<IParseCurrentUserController>();
+      mockCurrentUserController.Setup(obj => obj.IsCurrent(user)).Returns(true);
+      ParseCorePlugins.Instance.ObjectController = mockObjectController.Object;
+      ParseCorePlugins.Instance.CurrentUserController = mockCurrentUserController.Object;
+
+      return user.UnlinkFromAsync("parse", CancellationToken.None).ContinueWith(t => {
+        Assert.False(t.IsFaulted);
+        Assert.False(t.IsCanceled);
+        mockObjectController.Verify(obj => obj.SaveAsync(It.IsAny<IObjectState>(),
+          It.IsAny<IDictionary<string, IParseFieldOperation>>(),
+          It.IsAny<string>(),
+          It.IsAny<CancellationToken>()), Times.Exactly(1));
+        Assert.False(user.IsDirty);
+        Assert.NotNull(user.AuthData);
+        Assert.False(user.AuthData.ContainsKey("parse"));
+        Assert.AreEqual("some0neTol4v4", user.ObjectId);
+        Assert.AreEqual("ofWords", user["garden"]);
+      });
+    }
+
+    [Test]
+    [AsyncStateMachine(typeof(UserTests))]
+    public Task TestUnlinkNonCurrentUser() {
+      IObjectState state = new MutableObjectState {
+        ObjectId = "some0neTol4v4",
+        ServerData = new Dictionary<string, object>() {
+          { "sessionToken", "llaKcolnu" },
+          { "authData", new Dictionary<string, object> {
+            { "parse", new Dictionary<string, object>() }
+          }}
+        }
+      };
+      IObjectState newState = new MutableObjectState {
+        ServerData = new Dictionary<string, object>() {
+          { "garden", "ofWords" }
+        }
+      };
+      ParseUser user = ParseObject.FromState<ParseUser>(state, "_User");
+      var mockObjectController = new Mock<IParseObjectController>();
+      mockObjectController.Setup(obj => obj.SaveAsync(It.IsAny<IObjectState>(),
+          It.IsAny<IDictionary<string, IParseFieldOperation>>(),
+          It.IsAny<string>(),
+          It.IsAny<CancellationToken>())).Returns(Task.FromResult(newState));
+      var mockCurrentUserController = new Mock<IParseCurrentUserController>();
+      mockCurrentUserController.Setup(obj => obj.IsCurrent(user)).Returns(false);
+      ParseCorePlugins.Instance.ObjectController = mockObjectController.Object;
+      ParseCorePlugins.Instance.CurrentUserController = mockCurrentUserController.Object;
+
+      return user.UnlinkFromAsync("parse", CancellationToken.None).ContinueWith(t => {
+        Assert.False(t.IsFaulted);
+        Assert.False(t.IsCanceled);
+        mockObjectController.Verify(obj => obj.SaveAsync(It.IsAny<IObjectState>(),
+          It.IsAny<IDictionary<string, IParseFieldOperation>>(),
+          It.IsAny<string>(),
+          It.IsAny<CancellationToken>()), Times.Exactly(1));
+        Assert.False(user.IsDirty);
+        Assert.NotNull(user.AuthData);
+        Assert.True(user.AuthData.ContainsKey("parse"));
+        Assert.Null(user.AuthData["parse"]);
+        Assert.AreEqual("some0neTol4v4", user.ObjectId);
+        Assert.AreEqual("ofWords", user["garden"]);
+      });
     }
 
     [Test]
     [AsyncStateMachine(typeof(UserTests))]
     public Task TestLogInWith() {
-      // TODO (hallucinogen): do this
-      return Task.FromResult(0);
+      IObjectState state = new MutableObjectState {
+        ObjectId = "some0neTol4v4",
+        ServerData = new Dictionary<string, object>() {
+          { "sessionToken", "llaKcolnu" }
+        }
+      };
+      var mockController = new Mock<IParseUserController>();
+      mockController.Setup(obj => obj.LogInAsync("parse",
+          It.IsAny<IDictionary<string, object>>(),
+          It.IsAny<CancellationToken>())).Returns(Task.FromResult(state));
+      ParseCorePlugins.Instance.UserController = mockController.Object;
+
+      return ParseUser.LogInWithAsync("parse", new Dictionary<string, object>(), CancellationToken.None).ContinueWith(t => {
+        Assert.False(t.IsFaulted);
+        Assert.False(t.IsCanceled);
+        mockController.Verify(obj => obj.LogInAsync("parse",
+          It.IsAny<IDictionary<string, object>>(),
+          It.IsAny<CancellationToken>()), Times.Exactly(1));
+
+        var user = t.Result;
+        Assert.NotNull(user.AuthData);
+        Assert.NotNull(user.AuthData["parse"]);
+        Assert.AreEqual("some0neTol4v4", user.ObjectId);
+      });
     }
 
     [Test]
