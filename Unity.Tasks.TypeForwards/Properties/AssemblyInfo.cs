a7928e6b61795afc674780bf6bab2be266f86292
Stewart Miles <smiles@google.com>
2017-07-20 09:04:23 -0700

    Type forwarding DLL for the Parse Task implementation and Unity plugin build.
    This patch introduces:
    * Type forwarding DLL for Parse implementation of System.Threading.Tasks.
    * Project which builds a Unity plugin package that can be used to redistribute
      Parse's implementation of System.Threading.Tasks.
    
    Unity 2017 adds experimental support for .NET 4.5 (upgrading from a partially
    compatible version of .NET 3.5).  With .NET 4.5, the Parse Task implementation
    is no longer required as System.Threading.Task is present in the .NET
    framework.  For users distributing assemblies built against the Unity.Compat.dll
    and Unity.Tasks.dll components, this results in Unity failing to compile
    assemblies when .NET 4.5 is selected due to duplicate implementations of
    System.Threading.Tasks.
    
    So for the .NET 4.5 configuration in Unity, the Unity.Tasks.dll assembly (built
    by Unity.Tasks.TypeForwards) has been introduced which simply type forwards to
    the system implementation of System.Threading.Tasks.
    
    To standardize the location of Unity.Tasks.dll and Unity.Compat.dll for plugin
    developers reusing these components, a project has been introduced
    (Unity.Tasks.UnityPlugin) which builds a ParseTasks.unitypackage containing
    both the .NET 3.5 Parse implementation of System.Threading.Tasks and the
    .NET 4.5 type forwarding DLL.
    
    Finally, asset labels are used to mark .NET compatibilty of assemblies in the
    ParseTasks.unitypackage and asset metadata is used to disable the .NET 4.5
    type forwarding DLL by default allowing successful compilation when .NET 3.5 is
    selected in Unity.  The asset labels utilized to mark .NET compatibility of
    assemblies can be used by Unity editor plugins (e.g Google.VersionHandler.dll
    distributed as part of https://github.com/googlesamples/unity-jar-resolver) to
    enable / disable assemblies based upon the .NET framework version selected
    smoothing out the end user experience.

---
+++ Unity.Tasks.TypeForwards/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+// Copyright (c) 2015-present, Parse, LLC.  All rights reserved.  This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree.  An additional grant of patent rights can be found in the PATENTS file in the same directory.
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Parse")]
+[assembly: AssemblyDescription("Makes accessing services from Parse native and straightforward.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Parse")]
+[assembly: AssemblyCopyright("Copyright © Parse 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(true)]
