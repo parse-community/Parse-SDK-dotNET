3d5e3198e70497158469caeea0505de26028fc5b
bryane <bryane@moback.com>
2017-12-19 10:30:32 -0800

    Added type-forwarder for Unity.Compat to fix - https://groups.google.com/forum/#!topic/firebase-talk/2w1Lk62VIcg .

---
+++ Unity.Compat.TypeForwards/Public/CompatTypeForwards.cs
@@ -0,0 +1,13 @@
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedToAttribute(typeof(System.Tuple))]
+[assembly: TypeForwardedToAttribute(typeof(System.Tuple<,>))]
+[assembly: TypeForwardedToAttribute(typeof(System.Func<,,,,,>))]
+[assembly: TypeForwardedToAttribute(typeof(System.IProgress<>))]
+[assembly: TypeForwardedToAttribute(typeof(System.Runtime.CompilerServices.ConditionalWeakTable<,>))]
+[assembly: TypeForwardedToAttribute(typeof(System.Runtime.ExceptionServices.ExceptionDispatchInfo))]
+[assembly: TypeForwardedToAttribute(typeof(System.Linq.Expressions.IndexExpression))]
+[assembly: TypeForwardedToAttribute(typeof(System.Linq.Expressions.ExpressionVisitor))]
+[assembly: TypeForwardedToAttribute(typeof(System.Progress<>))]
+[assembly: TypeForwardedToAttribute(typeof(System.Threading.ThreadLocal<>))]
+[assembly: TypeForwardedToAttribute(typeof(System.Reflection.TypeInfo))]
