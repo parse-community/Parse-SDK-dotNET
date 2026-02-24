75b38169b62f1eb654a2f3d3c1d9fd75e3b5523e
theSlyest <theslyest@hotmail.com>
2026-02-24 15:04:59 +0000

    Added tests

---
+++ Parse.Tests/ClientTests.cs
@@ -0,0 +1,46 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parse.Infrastructure;
+
+namespace Parse.Tests;
+
+[TestClass]
+public class ClientTests
+{
+    [TestMethod]
+    public void TestParseClientConstructor()
+    {
+        ParseClient client = new("appId", "https://api.dummy-parse.com/1/", "dotnetKey");
+        Assert.AreEqual("appId", client.ServerConnectionData.ApplicationID);
+        Assert.AreEqual("https://api.dummy-parse.com/1/", client.ServerConnectionData.ServerURI);
+        Assert.AreEqual("dotnetKey", client.ServerConnectionData.Key);
+    }
+
+    [TestMethod]
+    public void TestPublicize()
+    {
+        ParseClient client = new("appId", "https://api.dummy-parse.com/1/", "dotnetKey");
+        client.Publicize();
+        Assert.AreSame(client, ParseClient.Instance);
+    }
+
+    [TestMethod]
+    public void TestConstructorWithInvalidUri() =>
+        // Should throw if using the old parse.com URI without Test=true
+        Assert.ThrowsExactly<InvalidOperationException>(() =>
+            new ParseClient("appId", "https://api.parse.com/1/", "dotnetKey") { });// Actually, looking at the code, it throws if the URI is EXACTLY "https://api.parse.com/1/"// and Test is not true.
+
+    [TestMethod]
+    public void TestConstructorWithTestTrue()
+    {
+        ServerConnectionData data = new()
+        { 
+            ApplicationID = "appId", 
+            ServerURI = "https://api.parse.com/1/", 
+            Key = "key", 
+            Test = true 
+        };
+        ParseClient client = new(data);
+        Assert.AreEqual("https://api.parse.com/1/", client.ServerConnectionData.ServerURI);
+    }
+}
---
+++ Parse.Tests/RoleTests.cs
@@ -0,0 +1,64 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parse.Infrastructure;
+using Parse.Platform.Objects;
+
+namespace Parse.Tests;
+
+[TestClass]
+public class RoleTests
+{
+    ParseClient Client { get; set; }
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        Client = new ParseClient(new ServerConnectionData { Test = true })
+        {
+        };
+        Client.Publicize();
+    }
+
+    [TestCleanup]
+    public void Clean() => (Client.Services as ServiceHub)?.Reset();
+
+    [TestMethod]
+    public void TestRoleConstructor()
+    {
+        ParseACL acl = new();
+        ParseRole role = new("Administrators", acl);
+        Assert.AreEqual("Administrators", role.Name);
+        Assert.AreEqual(acl, role.ACL);
+    }
+
+    [TestMethod]
+    public void TestRoleNameValidation()
+    {
+        ParseACL acl = new();
+        ParseRole role = new()
+        {
+            ACL = acl,
+
+            // Valid names
+            Name = "ValidName"
+        };
+        role.Name = "Valid-Name";
+        role.Name = "Valid_Name";
+        role.Name = "Valid Name";
+        role.Name = "123";
+
+        // Invalid names (should throw ArgumentException in OnSettingValue)
+        Assert.ThrowsExactly<ArgumentException>(() => role.Name = "Invalid@Name");
+        Assert.ThrowsExactly<ArgumentException>(() => role.Name = "Invalid#Name");
+    }
+
+    [TestMethod]
+    public void TestRoleNameImmutableAfterSave()
+    {
+        MutableObjectState state = new()
+        { ObjectId = "existingId" };
+        ParseRole role = Client.GenerateObjectFromState<ParseRole>(state, "_Role");
+        
+        Assert.ThrowsExactly<InvalidOperationException>(() => role.Name = "NewName");
+    }
+}
